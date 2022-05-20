04b9c421f3d24e3411bed1d1d36f288385149870
Meicle <meiclehaiara@outlook.com>
2022-05-20 10:56:47 -0300

    Delete ansy.py

--- ansi.py
+++
@@ -1,9 +0,0 @@
-class ANSI (): # terminal color changing codes
-    def red(letter):
-        return "\33[1;91m".format(letter=letter)
-    
-    def yellow(letter):
-        return "\33[1;93m".format(letter=letter)
-    
-    def green(letter):
-        return "\33[1;92m".format(letter=letter)
